2b069328d5cea4b222fe293b194b7b308b9a6f99
Nicolas Ruiz <nicolasruiz81@gmail.com>
2023-05-27 01:39:43 -0400

    Finished with testing health check endpoint

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::net::TcpListener;
+
+use actix_web::{HttpResponse, HttpServer, App, web};
+use actix_web::dev::Server;
+
+async fn health_check() ->  HttpResponse {
+    return HttpResponse::Ok().finish();
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+    
+    return Ok(server);
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    return run(listener)?.await;
+}
+

06df9ab332ef5711f40e30ea0116aa34e4060e68
Nicolas Ruiz <nicolasruiz81@gmail.com>
2023-05-28 02:24:25 -0400

    Added tdd for subscription endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,17 @@ use std::net::TcpListener;
 use actix_web::{HttpResponse, HttpServer, App, web};
 use actix_web::dev::Server;
 
+#[derive(serde::Deserialize)]
+struct Formdata {
+    email: String,
+    name: String
+}
+
 async fn health_check() ->  HttpResponse {
+    return HttpResponse::Ok().finish();
+}
+
+async fn subscribe(_form: web::Form<Formdata>) ->  HttpResponse {
     return HttpResponse::Ok().finish();
 }
 
@@ -11,6 +21,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
